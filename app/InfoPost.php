ca680d37c61b0c981a15a457afc1b1ebf634b149
Riccardo Ferrari <richi.f93@gmail.com>
2021-02-18 21:42:17 +0100

    relation functions

--- app/InfoPost.php
+++ app/InfoPost.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class InfoPost extends Model
 {
-    //
+    public function post() {
+        return $this->belongsTo('Appp\Post');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function infoPost() {
+        return $this->hasOne('App\InfoPost');
+    }
+
+    public function comments() {
+        return $this->hasMany('App\Comment');
+    }
 }
